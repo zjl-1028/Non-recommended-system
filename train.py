9bb4a76b6f4f8f5453f9680fe3f9df4f85409ba1
zjl-1028 <zhangjinglin@stu.bigc.edu.cn>
2026-05-13 08:31:04 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,75 +1,79 @@
 import torch
 import torch.nn as nn
-import json
-import re
-from torch.utils.data import Dataset, DataLoader
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader, random_split
 from model import FinalModel
 
-def clean_text(text):
-    text = text.lower()
-    text = re.sub(r'[^a-z0-9\s]', '', text)
-    return text.strip()
-
-def load_amazon_data():
-    data = []
-    path = "/mnt/ZQL/PBFL/Musical_Instruments_5.json"
-    with open(path, 'r', encoding='utf-8') as f:
-        for line in f:
-            d = json.loads(line)
-            uid = hash(d['reviewerID']) % 100000
-            iid = hash(d['asin']) % 100000
-            review = clean_text(d['reviewText'])
-            rating = float(d['overall'])
-            data.append([uid, iid, review, rating])
-    return data
-
 class ReviewDataset(Dataset):
-    def __init__(self, data, word2id, max_len=150):
-        self.data = data
-        self.word2id = word2id
+    def __init__(self, user_num, item_num, vocab_size, max_len=20):
+        self.user_num = user_num
+        self.item_num = item_num
+        self.vocab_size = vocab_size
         self.max_len = max_len
+        self.len = 10000
 
     def __len__(self):
-        return len(self.data)
+        return self.len
 
     def __getitem__(self, idx):
-        u, i, rev, rt = self.data[idx]
-        tokens = [self.word2id.get(w, 1) for w in rev.split()][:self.max_len]
-        tokens += [0] * (self.max_len - len(tokens))
-        return u, i, torch.LongTensor(tokens), torch.FloatTensor([rt])
+        u = torch.randint(0, self.user_num, (1,)).item()
+        i = torch.randint(0, self.item_num, (1,)).item()
+        ur = torch.randint(0, self.vocab_size, (self.max_len,))
+        ir = torch.randint(0, self.vocab_size, (self.max_len,))
+        rating = torch.rand(1).item() * 4 + 1
+        sarc = torch.randint(0, 2, (1,)).item()
+        return u, i, ur, ir, rating, sarc
 
-def build_vocab(data):
-    vocab = set()
-    for u, i, r, rt in data:
-        vocab.update(r.split())
-    word2id = {w: i+2 for i, w in enumerate(vocab)}
-    word2id['<pad>'] = 0
-    word2id['<unk>'] = 1
-    return word2id
+def get_data_loaders(user_num, item_num, vocab_size, batch_size=32):
+    dataset = ReviewDataset(user_num, item_num, vocab_size)
+    total_len = len(dataset)
+    train_len = int(0.8 * total_len)
+    val_len = int(0.1 * total_len)
+    test_len = total_len - train_len - val_len
+    train_ds, val_ds, test_ds = random_split(dataset, [train_len, val_len, test_len])
+    
+    train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_ds, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(test_ds, batch_size=batch_size, shuffle=False)
+    return train_loader, val_loader, test_loader
 
-if __name__ == "__main__":
-    device = torch.device('cuda')
-    amazon_data = load_amazon_data()
-    word2id = build_vocab(amazon_data)
-    dataset = ReviewDataset(amazon_data, word2id)
-    loader = DataLoader(dataset, batch_size=128, shuffle=True)
+def train():
+    user_num = 1000
+    item_num = 500
+    vocab_size = 5000
+    dim = 64
+    lr = 1e-3
+    epochs = 10
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    model = FinalModel(100000, 100000, len(word2id)).to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    train_loader, val_loader, test_loader = get_data_loaders(user_num, item_num, vocab_size)
+    model = FinalModel(user_num, item_num, vocab_size, dim).to(device)
     mse_loss = nn.MSELoss()
+    ce_loss = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=lr)
 
-    for epoch in range(20):
+    for epoch in range(epochs):
         model.train()
-        total_loss = 0
-        for u, i, rev, rt in loader:
-            u, i, rev = u.to(device), i.to(device), rev.to(device)
-            rt = rt.squeeze().to(device)
-
-            pred_score, _ = model(u, i, rev, rev)
-            loss = mse_loss(pred_score, rt)
-
+        train_loss = 0
+        for u, i, ur, ir, r, sc in train_loader:
+            u, i, ur, ir, r, sc = u.to(device), i.to(device), ur.to(device), ir.to(device), r.to(device), sc.to(device)
             optimizer.zero_grad()
+            pred_r, sarc_logits = model(u, i, ur, ir)
+            loss = mse_loss(pred_r, r) + ce_loss(sarc_logits, sc)
             loss.backward()
             optimizer.step()
-            total_loss += loss.item()
-        print(f"Epoch {epoch:2d} | Loss = {total_loss:.4f}")
+            train_loss += loss.item()
+
+        model.eval()
+        val_loss = 0
+        with torch.no_grad():
+            for u, i, ur, ir, r, sc in val_loader:
+                u, i, ur, ir, r, sc = u.to(device), i.to(device), ur.to(device), ir.to(device), r.to(device), sc.to(device)
+                pred_r, sarc_logits = model(u, i, ur, ir)
+                loss = mse_loss(pred_r, r) + ce_loss(sarc_logits, sc)
+                val_loss += loss.item()
+
+        print(f"Epoch {epoch+1} | Train Loss: {train_loss/len(train_loader):.4f} | Val Loss: {val_loss/len(val_loader):.4f}")
+
+if __name__ == '__main__':
+    train()
